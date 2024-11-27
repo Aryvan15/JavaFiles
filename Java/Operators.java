5fb59809656a228c0a23ddbdb93819c2403073d9
Aryvan15 <aryankhangsar15@gmail.com>
2024-11-27 21:35:50 +0530

    Added operator file to the folder

---
+++ Java/Operators.java
@@ -0,0 +1,19 @@
+public class Operators {
+    public static void main(String args[]){
+        int a= 10;
+        int d= 10;
+        int c= 0;
+        int b= 0;
+        //Post increment;
+        b= a++;
+        System.out.println("The value of b in pre increment: "+b);
+        System.out.println("The value of a in pre increment: "+a);
+
+        //Pre increment;
+        c= ++d;
+        System.out.println("The value of d in pre increment: "+d);
+        System.out.println("The value of c in pre Increment: "+c);
+        
+    }
+    
+}
